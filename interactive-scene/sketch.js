8c61e68c180e2c52987a2a9ddfa4194de83b2e89
Ch3rrygupta <cherryguptask@gmail.com>
2024-09-26 12:13:33 -0600

    start drawing guitar

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,10 +7,29 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
-  circle(13,14,90);
+  background(228, 202, 152);
+  drawGuitarBackground()
+  drawGuitarStrings()
+  
 }
+
+function drawGuitarBackground() {
+  fill(16,16,16)
+  ellipse (300, 210, 220, 190)
+}
+
+function drawGuitarStrings() {
+  fill (97, 83, 73)
+  rect(0, 150, 600, 3)
+  rect(0, 173, 600, 3)
+  rect(0, 196, 600, 4)
+  fill(75, 59, 46)
+  rect(0, 220, 600, 5)
+  rect(0, 245, 600, 6)
+  rect(0, 271, 600, 7)
+}
+
