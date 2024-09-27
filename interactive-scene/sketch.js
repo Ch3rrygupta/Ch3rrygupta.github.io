1854f85d89f48afa6a2663c5aa1f85f3c277d491
Ch3rrygupta <cherryguptask@gmail.com>
2024-09-27 14:15:48 -0600

    drew buttons on guitar

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -13,7 +13,9 @@ function setup() {
 function draw() {
   background(228, 202, 152);
   drawGuitarBackground()
+  drawChordButtons()
   drawGuitarStrings()
+  
   
 }
 
@@ -31,5 +33,30 @@ function drawGuitarStrings() {
   rect(0, 220, 600, 5)
   rect(0, 245, 600, 6)
   rect(0, 271, 600, 7)
+}
+
+function drawChordButtons(){
+  // I learned how to change the opacity of a filled shape here!
+  fill(190, 190, 190, 110);
+  rect(74, 50, 50, 300, 10)
+  rect(141, 50, 50, 300, 10)
+  rect(208, 50, 50, 300, 10)
+  rect(275, 50, 50, 300, 10)
+  rect(342, 50, 50, 300, 10)
+  rect(409, 50, 50, 300, 10)
+  rect(476, 50, 50, 300, 10)
+  // I learned how to add text to a scene here.
+  textSize(30)
+  fill(88, 16, 16)
+  text('Em', 76, 100);
+  text('Am', 143, 100);
+  text('Dm', 210, 100);
+  text('G', 277, 100);
+  text('C', 344, 100);
+  text('F', 412, 100);
+  text('Bb', 478, 100);
+
+
+
 }
 
