c412f3f4e1d10c50b6c948eac40d1e9041ca7214
Ch3rrygupta <cherryguptask@gmail.com>
2024-10-08 14:06:51 -0600

    Started the object array folder and added a detailed idea of  what my project is going to be on the comment header

---
+++ array-object/sketch.js
@@ -0,0 +1,20 @@
+// Cherry Gupta
+// Arrays and Object Notation Project
+// October 21, 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// project idea: I'm going to try to make the water sort game that I'm obsessed with playign on my phone. 
+// how the game works is you try to get all the same colored water together but you cant stack different colored waters together.
+// there will be two empty water cups and the empty ones you can put any color into
+// the goal is to make all of the test tubes the same color of water and I'll try to make a little confetti everytime the tubes become completely sorted
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
