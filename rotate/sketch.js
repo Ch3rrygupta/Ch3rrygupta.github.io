536c152ccbd7ed83e84949286c7c4e6fd67c77f6
Gupta0101577 <0101577@spsd.sk.ca>
2024-10-17 14:01:19 -0600

    rotate demo

---
+++ rotate/sketch.js
@@ -0,0 +1,24 @@
+// translae rotate
+// Cherry
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(220);
+
+  push();
+
+  translate(200, 200);
+  rotate (mouseX);
+  fill("red");
+  rect(0, 0, 100, 100);
+  
+  pop();
+  fill("green");
+  rect(width/2, height-200, width*2, 400);
+}
+
