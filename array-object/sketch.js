12185897f1de4c14f870fc45fcb25a2e5f2b8074
Ch3rrygupta <cherryguptask@gmail.com>
2024-10-20 20:03:28 -0600

    tried a whole new logic which actually ended up working, basically coded the whole game today, removed sound functions, changed window size to select size for the tubes

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,53 +5,141 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// project idea: I'm going to try to make the water sort game that I'm obsessed with playign on my phone. 
-// how the game works is you try to get all the same colored water together but you cant stack different colored waters together.
-// there will be two empty water cups and the empty ones you can put any color into
-// the goal is to make all of the test tubes the same color of water and I'll try to make a little confetti everytime the tubes become completely sorted
 
+let glasses = [];
+let colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+let currentGlass = null;
+let gameWon = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 600);
+    let glassWidth = 100;
+    let glassHeight = 200;
+    let glassMargin = 20;
+    let totalGlasses = 7; // Total glasses
+
+    // Calculate starting x position to center the glasses
+    let totalWidth = totalGlasses * glassWidth + (totalGlasses - 1) * glassMargin;
+    let startX = (width - totalWidth) / 2;
+
+    // Initialize glasses, with the last two being empty
+    for (let i = 0; i < totalGlasses; i++) {
+        let glass = new Glass(startX + i * (glassWidth + glassMargin), height - glassHeight - 50, glassWidth, glassHeight); // Raise glasses by 50 pixels
+        
+        // Fill with colored water only for the first five glasses
+        if (i < 5) {
+            for (let j = 0; j < 3; j++) { // Fill with 3 waters each
+                glass.fillWater(random(colors));
+            }
+        }
+        
+        glasses.push(glass);
+    }
 }
 
 function draw() {
-  background(100);
-  drawTubes();
+    background(220);
+    
+    // Draw each glass
+    for (let glass of glasses) {
+        glass.draw();
+    }
+    
+    // Check if the game is won
+    if (gameWon) {
+        displayWinningMessage();
+    }
 }
 
-
-// this function will draw the tubes at a specific hieght and width along the screen
-function drawTubes(){
-  fill(255, 255, 255, 50);
-  rect(width/9, height/5, 50, 200, 10);
-  rect(width/9 *3, height/5, 50, 200, 10);
-  rect(width/9 *5, height/5, 50, 200, 10);
-  rect(width/9 *7, height/5, 50, 200, 10);
-  rect(width/9, height/5 *3, 50, 200, 10);
-  rect(width/9 *3, height/5 *3, 50, 200, 10);
-  rect(width/9 *5, height/5 *3, 50, 200, 10);
-  rect(width/9 *7, height/5 *3, 50, 200, 10);
+function mousePressed() {
+    if (!gameWon) { // Only allow interaction if the game is not won
+        // Check which glass is clicked
+        for (let glass of glasses) {
+            if (glass.isMouseOver()) {
+                if (currentGlass === null) {
+                    currentGlass = glass; // Select the glass
+                } else {
+                    // Try to pour water from current glass to the clicked glass
+                    if (glass !== currentGlass && glass.canPour(currentGlass)) {
+                        glass.pour(currentGlass);
+                        checkWinCondition(); // Check if the game is won after pouring
+                    }
+                    currentGlass = null; // Deselect after pouring
+                }
+                break;
+            }
+        }
+    }
 }
 
-// function that will create balls in the tubes
-function createBalls(){
-  let theBalls = {
-    x: 160,
-    y: 150,
-    radius: 15,
-    alpha: 4, 
-  };
+function checkWinCondition() {
+    // Check if all glasses are sorted
+    let allSorted = true;
+
+    for (let glass of glasses) {
+        if (glass.water.length > 0) {
+            let color = glass.water[0];
+            for (let waterColor of glass.water) {
+                if (waterColor !== color) {
+                    allSorted = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (allSorted) {
+        gameWon = true;
+    }
 }
 
-// function that will play a "boop" sound whenever a bal is picked up or dropped into a tube
-function playSound(){
-  
+function displayWinningMessage() {
+    fill(0);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("You Won!", width / 2, height / 2);
 }
 
+// Glass class to manage water
+class Glass {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.water = [];
+    }
 
-// function that will load the sound
-function preload(){
+    fillWater(color) {
+        if (this.water.length < 5) { // Limit water levels
+            this.water.push(color);
+        }
+    }
 
+    draw() {
+        fill(255);
+        rect(this.x, this.y, this.w, this.h);
+        this.drawWater();
+    }
+
+    drawWater() {
+        for (let i = 0; i < this.water.length; i++) {
+            fill(this.water[i]);
+            rect(this.x, this.y + this.h - (i + 1) * (this.h / 5), this.w, this.h / 5);
+        }
+    }
+
+    isMouseOver() {
+        return mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h;
+    }
+
+    canPour(fromGlass) {
+        return this.water.length < 5 && (this.water.length === 0 || this.water[this.water.length - 1] === fromGlass.water[fromGlass.water.length - 1]);
+    }
+
+    pour(fromGlass) {
+        if (fromGlass.water.length > 0) {
+            this.fillWater(fromGlass.water.pop());
+        }
+    }
 }
-
