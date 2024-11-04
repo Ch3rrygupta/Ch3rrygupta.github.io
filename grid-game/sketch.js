9587d01fb78df447f59d6e5e9a954086a64d1c70
Ch3rrygupta <cherryguptask@gmail.com>
2024-11-04 14:13:19 -0600

    added the global variables that I am going to use later, I also added an image for pac instead of drawing it, and I added the key function to move pac around

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -13,6 +13,22 @@
 // the player also has to avoid thhe ghosts by moving around
 
 
+let pac;
+let grid = [];
+let rows, cols;
+const w = 20; //cell length (standard w = 10)
+let speedX = 0;
+let speedY = 0;
+let totalScore = 0;
+let p;
+let ghosts = [];
+let ghostNum = 4;
+let r;
+let thetaoff = 0;
+let dir; //equals 0 if up arrow is pressed, 1 if right arrow is pressed, 2 if down arrow is pressed, 3 if left arrow is pressed
+let neighbors = [];
+let pacImg;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -21,9 +37,26 @@ function draw() {
   background(220);
 }
 
+function preload(){
+  pacImg = loadImage("pac.img");
+}
 
-function drawPac(){
-  noStroke();
-  fill("yellow");
-  circle(x, y, 60);
+
+function keyPressed() {
+  if (key === "w") {
+    //move up
+    movePac(pac.x, pac.y - 1);
+  }
+  if (key === "s") {
+    //move down
+    movePac(pac.x, pac.y + 1);
+  }
+  if (key === "d") {
+    //move right
+    movePac(pac.x + 1, pac.y);
+  }
+  if (key === "a") {
+    //move left
+    movePac(pac.x - 1, pac.y);
+  }
 }
