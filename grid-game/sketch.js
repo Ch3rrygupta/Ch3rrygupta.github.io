2685712750d291c51d136e64d1657ab82c76d265
Ch3rrygupta <cherryguptask@gmail.com>
2024-11-15 13:51:33 -0600

    Deleted the files That I didnt need and I added  spaces and comments. I'm all done now!

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,23 +1,19 @@
 // Grid Based Game
 // Cherry Gupta
-// October 30, 2024
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// November 15, 2024
 
-
-// Project ideas and brainstorming:
-// pacman 
-// I will do this by making the player control pac with WASD and the ghosts will move around randomly
-//  the goal is for the player to collect all the food and coins before going back home 
-// the player also has to avoid thhe ghosts by moving around
-
+// Project Description:
+// I coded a pacman inspired game where the user controls the yellow circle with the arrow keys
+// and has to try to eat all the smaller circles while avoiding the ghosts (other colored circles)
 
 // extra for experts:
-// for my extra for experts I made class and played around with that. we also got shown bthis in class recentkly but I 
-// coded most of the class stuff before ot was shown in class and I've been playinf aroufdn wiht it in my other projects as well.
+// 1. I used OOP and made classes within the game to clean up the draw loop.
+//    We were shown this in class recently but I have been playing around with this since the beginning of the semester, 
+//    and I added it to my code before we were shown in class :))
+// 2. In this project I also played around with frame speed to make the pacman and the ghots go slower.
+//    I found that without doing this, the characters would move really fast and make them difficult to keep track of and control
 
-
+// Initializing grid measurments
 let rows = 15;
 let cols = 15;
 let gridSize = 40;
@@ -27,7 +23,7 @@ let pacman;
 let game;
 let ghosts = [];
 let pacmanFrameRateDivider = 10; // Slows down Pac-Man's movement
-let ghostFrameRateDivider = 30; // Slows down Ghosts' movement
+let ghostFrameRateDivider = 30; // Slows down Ghosts' movement to be slower than pac, this makes it easier for the user to win 
 
 // Pac-Man class
 class Pacman {
@@ -38,6 +34,7 @@ class Pacman {
     this.dirY = 0;
   }
 
+  // will let the pacman move around the grid
   move(grid) {
     let newX = this.x + this.dirX;
     let newY = this.y + this.dirY;
@@ -53,7 +50,8 @@ class Pacman {
       }
     }
   }
-
+ 
+  // draws the pacman
   draw() {
     fill(255, 255, 0);
     ellipse(
@@ -72,14 +70,23 @@ class Ghost {
     this.color = color;
   }
 
+  
   move(grid, pacmanX, pacmanY) {
     let possibleMoves = [];
 
-    // Check all possible moves
-    if (grid[this.y - 1][this.x] !== 1) possibleMoves.push({ x: this.x, y: this.y - 1 });
-    if (grid[this.y + 1][this.x] !== 1) possibleMoves.push({ x: this.x, y: this.y + 1 });
-    if (grid[this.y][this.x - 1] !== 1) possibleMoves.push({ x: this.x - 1, y: this.y });
-    if (grid[this.y][this.x + 1] !== 1) possibleMoves.push({ x: this.x + 1, y: this.y });
+    // Check all possible moves for the ghosts
+    if (grid[this.y - 1][this.x] !== 1) {
+      possibleMoves.push({ x: this.x, y: this.y - 1 });
+    }
+    if (grid[this.y + 1][this.x] !== 1) {
+      possibleMoves.push({ x: this.x, y: this.y + 1 });
+    }
+    if (grid[this.y][this.x - 1] !== 1) {
+      possibleMoves.push({ x: this.x - 1, y: this.y });
+    }
+    if (grid[this.y][this.x + 1] !== 1) {
+      possibleMoves.push({ x: this.x + 1, y: this.y });
+    }
 
     // Pick the move that brings the ghost closer to Pac-Man
     let bestMove = possibleMoves[0];
@@ -93,7 +100,7 @@ class Ghost {
       }
     }
 
-    // Update position
+    // Update the ghost position
     this.x = bestMove.x;
     this.y = bestMove.y;
   }
@@ -119,27 +126,31 @@ class Game {
     this.initGrid();
   }
 
+  // set up the grid
   initGrid() {
     for (let y = 0; y < this.rows; y++) {
       this.grid[y] = [];
       for (let x = 0; x < this.cols; x++) {
         if (x === 0 || y === 0 || x === this.cols - 1 || y === this.rows - 1 || random() < 0.2) {
           this.grid[y][x] = 1; // Wall
-        } else {
-          this.grid[y][x] = 0; // Dot
+        }
+        else {
+          this.grid[y][x] = 0; // food dot
         }
       }
     }
     this.grid[pacman.y][pacman.x] = 0; // Clear Pac-Man's starting position
   }
 
+  // draw the grid
   drawGrid() {
     for (let y = 0; y < this.rows; y++) {
       for (let x = 0; x < this.cols; x++) {
         if (this.grid[y][x] === 1) {
           fill(50, 50, 200); // Wall
           rect(x * gridSize, y * gridSize, gridSize, gridSize);
-        } else if (this.grid[y][x] === 0) {
+        } 
+        else if (this.grid[y][x] === 0) {
           fill(200, 200, 0); // Dot
           ellipse(
             x * gridSize + gridSize / 2,
@@ -187,65 +198,87 @@ function setup() {
 }
 
 function draw() {
+  // make the game won screen if won
   if (game.won) {
     background(0);
     fill(255);
     textAlign(CENTER, CENTER);
-    textSize(32);
-    text("You Won!", width / 2, height / 2);
+    textSize(50);
+    text("You Won!", width / 2, height / 2 - 25);
+    text("Refresh to play again :)", width / 2 , height / 2 + 60);
     noLoop(); // Stop the game loop
     return;
   }
-
+  
+  // make the game lost screen if lost
   if (game.lost) {
     background(0);
     fill(255, 0, 0);
     textAlign(CENTER, CENTER);
-    textSize(32);
-    text("You Lose!", width / 2, height / 2);
+    textSize(50);
+    text("You Lost!", width / 2, height / 2 - 25);
+    text("Refresh to play again :)", width / 2 , height / 2 + 60);
     noLoop(); // Stop the game loop
     return;
   }
 
+  // set bg color
   background(0);
 
+  // draw the grid
   game.drawGrid();
 
+  // makes the pacman go slower
   if (frameCount % pacmanFrameRateDivider === 0) {
     pacman.move(game.grid);
   }
 
+  // makes the ghost go slower
   if (frameCount % ghostFrameRateDivider === 0) {
     for (let ghost of ghosts) {
       ghost.move(game.grid, pacman.x, pacman.y);
     }
   }
 
+  // draw the characters
   pacman.draw();
   for (let ghost of ghosts) {
     ghost.draw();
   }
 
+  // check if the game is won
   if (game.checkWin()) {
     game.won = true;
   }
 
+  // chcek if the game is lost
   if (game.checkLose()) {
     game.lost = true;
   }
 }
 
+// checking which key is pressed to move the pacman around
 function keyPressed() {
+  // up key
   if (keyCode === UP_ARROW) {
     pacman.dirX = 0;
     pacman.dirY = -1;
-  } else if (keyCode === DOWN_ARROW) {
+  } 
+
+  // down key
+  else if (keyCode === DOWN_ARROW) {
     pacman.dirX = 0;
     pacman.dirY = 1;
-  } else if (keyCode === LEFT_ARROW) {
+  } 
+
+  // left key
+  else if (keyCode === LEFT_ARROW) {
     pacman.dirX = -1;
     pacman.dirY = 0;
-  } else if (keyCode === RIGHT_ARROW) {
+  } 
+
+  // right key
+  else if (keyCode === RIGHT_ARROW) {
     pacman.dirX = 1;
     pacman.dirY = 0;
   }
